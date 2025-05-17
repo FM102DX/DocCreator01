d1cb6a170813bf9bbbb3386bfb4892d320400e24
FM102DX <78497211+FM102DX@users.noreply.github.com>
2025-05-17 09:41:38 +0300

    Сделал чтобы настройки были синхронными и нормално сохранялись

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -8,6 +8,7 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using DocCreator01.Contracts;
 using DocCreator01.Services;
+using DocCreator01.Messages;
 
 namespace DocCreator01.ViewModels
 {
@@ -17,10 +18,13 @@ namespace DocCreator01.ViewModels
         private readonly IDirtyStateManager _dirtyStateMgr;
         private readonly IProjectHelper _projectHelper;
         private readonly CompositeDisposable _cleanup = new CompositeDisposable();
+        public Guid Id { get; set; }= Guid.NewGuid(); 
 
         [Reactive] public IEnumerable<HtmlGenerationProfile> HtmlGenerationProfiles { get; set; }
         [Reactive] public HtmlGenerationProfile? SelectedHtmlGenerationProfile { get; set; }
         [Reactive] public GenerateFileTypeEnum GenDocType { get; set; }
+
+        private bool _suppressBroadcast; 
 
         public SettingsViewModel(Settings model, IProjectHelper projectHelper, IDirtyStateManager dirtyStateMgr = null)
         {
@@ -31,36 +35,63 @@ namespace DocCreator01.ViewModels
             GenDocType = Model.GenDocType;
             HtmlGenerationProfiles = _projectHelper.GetHtmlGenerationProfiles();
             SelectedHtmlGenerationProfile = Model.CurrentHtmlGenerationProfile;
+
+            //this.SelectedHtmlGenerationProfile
             this.WhenAnyValue(x => x.SelectedHtmlGenerationProfile)
-                .Skip(1) // Skip initial set, only react to user changes
+               // .Skip(1) // Skip initial set, only react to user changes
                 .Subscribe(profile =>
                 {
-                    if (Model.CurrentHtmlGenerationProfile != profile) // Update only if changed
-                    {
-                        Model.CurrentHtmlGenerationProfile = profile;
-                        Model.CurrentHtmlGenerationProfileId = profile?.Id ?? 0;
-                        _dirtyStateMgr.MarkAsDirty();
-                    }
+                    Model.CurrentHtmlGenerationProfile = profile;
+                    Model.CurrentHtmlGenerationProfileId = profile?.Id ?? 0;
+                    _dirtyStateMgr.MarkAsDirty();
+                    Publish(nameof(SelectedHtmlGenerationProfile), profile);
                 })
                 .DisposeWith(_cleanup);
-            
-            // When GenDocType changes, update the model
+
+            //this.GenDocType
             this.WhenAnyValue(x => x.GenDocType)
-                .Skip(1) // Skip initial set, only react to user changes
+             //   .Skip(1) // Skip initial set, only react to user changes
                 .Subscribe(type =>
                 {
-                    if (Model.GenDocType != type) // Update only if changed
-                    {
-                        Model.GenDocType = type;
-                        _dirtyStateMgr.MarkAsDirty();
-                    }
+                    Model.GenDocType = type;
+                    _dirtyStateMgr.MarkAsDirty();
+                    Publish(nameof(GenDocType), type);
                 })
                 .DisposeWith(_cleanup);
+
+            // SUBSCRIBE to other VMs
+            MessageBus.Current.Listen<SettingsChangedMessage>()
+                .Where(m => !ReferenceEquals(m.Sender, this))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(m =>
+                {
+                    _suppressBroadcast = true;                  // prevent loop
+                    switch (m.PropertyName)
+                    {
+                        case nameof(GenDocType):
+                            GenDocType = (GenerateFileTypeEnum)m.Value!;
+                            break;
+                        case nameof(SelectedHtmlGenerationProfile):
+                            SelectedHtmlGenerationProfile = (HtmlGenerationProfile?)m.Value;
+                            break;
+                    }
+                    _suppressBroadcast = false;
+                })
+                .DisposeWith(_cleanup);
+
         }
 
         public void Dispose() => _cleanup.Dispose();
 
         // IDirtyTrackable implementation
         public IDirtyStateManager DirtyStateMgr => _dirtyStateMgr;
+
+        private void Publish(string prop, object? val)
+        {
+            if (_suppressBroadcast) return;                 // don’t echo externally-initiated changes
+            MessageBus.Current.SendMessage(
+                new SettingsChangedMessage(prop, val, this));
+        }
+
     }
 }

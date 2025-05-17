b7d626cc137f7734903517817a7b1ab99e554793
FM102DX <78497211+FM102DX@users.noreply.github.com>
2025-05-17 07:35:00 +0300

    Сделал прокидку профиля генерации хтмл в комбобокс

--- Models/HtmlGenerationProfile.cs
+++ Models/HtmlGenerationProfile.cs
@@ -1,20 +1,22 @@
 using DocCreator01.Models;
-using System;
 
-namespace DocCreator01.Models
+public class HtmlGenerationProfile : IEquatable<HtmlGenerationProfile>
 {
-    public class HtmlGenerationProfile
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public int HtmlFontSize { get; set; } = 14;
-        public ElementSpacingInfo HtmlH1Margins { get; set; }
-        public ElementSpacingInfo HtmlH2Margins { get; set; }
-        public ElementSpacingInfo HtmlH3Margins { get; set; }
-        public ElementSpacingInfo HtmlH4Margins { get; set; }
-        public ElementSpacingInfo HtmlH5Margins { get; set; }
-        public ElementSpacingInfo HtmlTableCellPaddings { get; set; }
-        public string TableHeaderColor { get; set; } = "#F1F3F6";
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int HtmlFontSize { get; set; } = 14;
+    public ElementSpacingInfo HtmlH1Margins { get; set; }
+    public ElementSpacingInfo HtmlH2Margins { get; set; }
+    public ElementSpacingInfo HtmlH3Margins { get; set; }
+    public ElementSpacingInfo HtmlH4Margins { get; set; }
+    public ElementSpacingInfo HtmlH5Margins { get; set; }
+    public ElementSpacingInfo HtmlTableCellPaddings { get; set; }
+    public string TableHeaderColor { get; set; } = "#F1F3F6";
 
+    public override bool Equals(object obj) => Equals(obj as HtmlGenerationProfile);
+
+    public bool Equals(HtmlGenerationProfile other)
+        => other != null && Id == other.Id;
+
+    public override int GetHashCode() => Id.GetHashCode();
 }
